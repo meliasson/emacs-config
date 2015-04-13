063f91a4873a933cd79ad6d480218393dd137b0d
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2015-04-13 08:18:05 +0200

    Env variable HOME instead of tilde in installer.

--- installer.sh
+++ installer.sh
@@ -2,9 +2,9 @@
 
 # Clone Emacs settings.
 SETTINGS_URL="https://github.com/meliasson/emacs-mini.git"
-SETTINGS_DIR="~/.emacs.s"
+SETTINGS_DIR="$HOME/.emacs.s"
 /usr/bin/env git clone $SETTINGS_URL $SETTINGS_DIR
 
 # Create symlink.
-rm ~/.emacs.d/init.el
-ln -s $SETTINGS_DIR/init.el ~/.emacs.d/personal/init.el
+rm $HOME/.emacs.d/init.el
+ln -s $SETTINGS_DIR/init.el $HOME/.emacs.d/personal/init.el
