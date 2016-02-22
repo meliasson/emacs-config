6e895ebed3905ee46160f2c7006231b8fddc5b57
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2016-02-22 23:09:24 +0100

    Add autoindenting, commenting and Smex

--- init.el
+++ init.el
@@ -23,18 +23,47 @@
 ;; Show matching parens.
 (show-paren-mode t)
 
-;; Delete trailing whitespace on save.
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; Do whitespace cleanup on save.
+(add-hook 'before-save-hook 'whitespace-cleanup)
 
 ;; y or n.
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Bind commenting.
+(global-set-key (kbd "C-c c") 'comment-or-uncomment-region)
+
+;; Bind cleaning.
+(global-set-key (kbd "C-c n") 'simple-clean-region-or-buffer)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Clean region or buffer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun simple-clean-buffer ()
+  "Indent the currently visited buffer."
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun simple-clean-region-or-buffer ()
+  "Cleans a region if selected, otherwise the whole buffer."
+  (interactive)
+  (save-excursion
+    (if (region-active-p)
+        (progn
+          (indent-region (region-beginning) (region-end))
+          (whitespace-cleanup)
+          (message "Cleaned selected region."))
+      (progn
+        (simple-indent-buffer)
+        (whitespace-cleanup)
+        (message "Cleaned buffer.")))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Package management
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; List packages to install if missing.
-(setq package-list '(flycheck))
+(setq package-list '(flycheck smex))
 
 ;; List repositories containing the packages.
 (setq package-archives '(("elpa" . "http://tromey.com/elpa/")
@@ -67,7 +96,14 @@
 ;; Ido
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-package-enable-at-startup
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Smex
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(global-set-key (kbd "M-x") 'smex)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
