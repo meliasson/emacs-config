f3194ef5c49335b893dea68dfdb301ad055ddcab
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2017-08-26 20:13:17 +0200

    Enable web mode in ejs files

--- init.el
+++ init.el
@@ -90,6 +90,7 @@
   :ensure t
   :config
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
