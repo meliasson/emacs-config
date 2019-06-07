3366825608b738740a246f16861d4f8ca3513525
Mikael Eliasson <mikael.eliasson@mittmedia.se>
2019-06-07 10:46:51 +0200

    Be consistent in comments

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SETTINGS_URL="https://github.com/meliasson/emacs-mini.git"
 SETTINGS_DIR="$HOME/.emacs.s"
 /usr/bin/env git clone $SETTINGS_URL $SETTINGS_DIR
 
-# Create directory
+# Create directory.
 mkdir -p $HOME/.emacs.d
 
 # Create symlink.
