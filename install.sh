ddf26fa10eafb9fe30739dc3a1b1a277110152c0
Mikael Eliasson <mikael.e.eliasson@gmail.com>
2023-12-29 09:31:33 +0100

    Fix install script
    HTTPS instead of SSH plus some variable renaming
    leftovers.

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Clone config.
-CONFIG_URL="git@github.com:meliasson/emacs-config.git"
+CONFIG_URL="https://github.com/meliasson/emacs-config.git"
 CONFIG_DIR="$HOME/.emacs.c"
-/usr/bin/env git clone $SETTINGS_URL $SETTINGS_DIR
+/usr/bin/env git clone $CONFIG_URL $CONFIG_DIR
 
 # Create directory.
 mkdir -p $HOME/.emacs.d
@@ -13,4 +13,4 @@ if [ -f $HOME/.emacs.d/init.el ]
 then
     rm $HOME/.emacs.d/init.el
 fi
-ln -s $SETTINGS_DIR/init.el $HOME/.emacs.d/init.el
+ln -s $CONFIG_DIR/init.el $HOME/.emacs.d/init.el
